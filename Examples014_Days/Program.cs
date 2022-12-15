1173ac51748a43f771303263e3307837a73dd01b
Mike-P63 <rumaish@rambler.ru>
2022-12-15 23:19:06 +0400

    add Example 014 Days

---
+++ Examples014_Days/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine("Введите цифру дня недели");
+int x = int.Parse(Console.ReadLine());
+
+if (x == 1)
+{
+    Console.Write("Понедельник");
+}
+
+else if (x == 2)
+{
+    Console.Write("Вторник");    
+}
+else if (x == 3)
+{
+    Console.Write("Среда");
+}
+else if (x == 4)
+{
+    Console.Write("Четверг");
+}
+else if (x == 5)
+{
+    Console.Write("Пятница");
+}
+else if (x == 6)
+{
+    Console.Write("Суббота");
+}
+else if (x == 7)
+{
+    Console.Write("Воскресенье");
+}
+if (x < 1 || x > 7);
+{
+    Console.Write("Ввели не корректный день недели");
+}
