1ccdd86e0ad028bb79f89cc1d6e2dde103475723
Mike-P63 <rumaish@rambler.ru>
2022-12-15 23:16:29 +0400

    add Example 016

---
+++ Example016_Task_7/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+Console.WriteLine("Введите трехзначное число ");
+int number=int.Parse(Console.ReadLine()!);
+if (number < 0)
+{number=number*(-1);}
+Console.WriteLine(number%10);
