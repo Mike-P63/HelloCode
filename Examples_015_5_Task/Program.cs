897f1aac1863aa810a6df958b0ef4fecd005677a
Mike-P63 <rumaish@rambler.ru>
2022-12-15 22:42:31 +0400

    add Example 015

---
+++ Examples_015_5_Task/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число");
+int numberN = int.Parse(Console.ReadLine()!);
+int numberC = -numberN;
+while (numberC<=numberN)
+{
+    Console.Write(numberC+" ");
+    numberC++;
+}
