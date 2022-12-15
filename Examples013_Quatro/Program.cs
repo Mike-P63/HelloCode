81e63a7fcb8263b09549d78ee5b9586745ae8d27
Mike-P63 <rumaish@rambler.ru>
2022-12-15 21:57:48 +0400

    add Example 013 Quatro

---
+++ Examples013_Quatro/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+Console.WriteLine("введите число 1");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("введите число 2");
+int num2 = int.Parse(Console.ReadLine());
+int sq1 = Sq(num1);
+int sq2 = Sq(num2);
+
+if (sq1 == num2 || sq2 == num1)
+{
+Console.Write("Да");
+}
+else 
+{
+  Console.Write("Нет");  
+}
+
+int Sq (int a)
+{
+    return a * a;
+}
