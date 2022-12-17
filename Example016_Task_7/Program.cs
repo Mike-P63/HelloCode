9f7e4c7fca3a32ec2e75dffb233d30a745ece098
Mike-P63 <rumaish@rambler.ru>
2022-12-18 00:41:45 +0400

    Made changes in Examles 016 middle number of 3 numerics number

--- Example016_Task_7/Program.cs
+++ Example016_Task_7/Program.cs
@@ -1,6 +1,19 @@
 ﻿Console.Clear();
 Console.WriteLine("Введите трехзначное число ");
-int number=int.Parse(Console.ReadLine()!);
-if (number < 0)
-{number=number*(-1);}
-Console.WriteLine(number%10);
+int number = int.Parse(Console.ReadLine()!);
+int amount = number.ToString().Length;
+int InCenter(int a)
+{
+    int result = ((a / 10) % 10);
+    return result;
+}
+if (amount < 3 || amount > 3)
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+}
+else
+{
+    Console.WriteLine(InCenter(number));
+}
+
+
