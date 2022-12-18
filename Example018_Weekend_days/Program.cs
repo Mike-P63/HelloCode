605a610a1043dee8a3f935ad29bcc6576f482d7a
Mike-P63 <rumaish@rambler.ru>
2022-12-18 20:52:44 +0400

    Add Examle 018 Weekend Days

---
+++ Example018_Weekend_days/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Введите номер дня недели");
+int number = int.Parse(Console.ReadLine()!);
+if (number == 6 || number  == 7)
+{
+    Console.Write("Под цифрой " + number + " - Выходной день");
+}
+else
+{
+    Console.Write("Под цифрой " + number + " - Рабочий день");
+}
