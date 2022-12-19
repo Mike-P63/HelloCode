5299676695bd7ffc14df929158c3a303d1765429
Mike-P63 <rumaish@rambler.ru>
2022-12-19 11:22:25 +0400

    Add Examle 020 Weekenddays

---
+++ Example020_Weekenddays/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите число");
+int x = int.Parse(Console.ReadLine()!);
+
+if (x < 100)
+ {
+    Console.Write("третьей цифры нет");
+ }
+else
+{
+while (x >= 1000)
+{
+    x = x / 10;
+}
+
+if (x < 1000)
+ { 
+    int three = x % 100;
+    int a = three % 10;
+    Console.Write(a);
+ }
+ }
